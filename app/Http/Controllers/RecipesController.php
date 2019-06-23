df1b83c448513983400feafe8722fbe654ae8bae
Bola Bally <bolabally@gmail.com>
2019-06-23 15:38:48 +0000

    addRecipesController.php

--- app/Http/Controllers/RecipesController.php
+++ app/Http/Controllers/RecipesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Recipe;
 
 class RecipesController extends Controller
 {
@@ -11,5 +12,43 @@ class RecipesController extends Controller
 	foreach($recipes as $recipe){
 	    echo $recipe;
     }
-}
+
+    }
+
+    public function create(){
+
+    Recipe::create(
+          [
+		   'title'=>'Jellof',
+		   'ingredients'=>'ingredients',
+		   'method'=>'method',
+		   'size'=>'size',
+		   'time'=>'time'
+		  ]
+	);
+	return "created";
+    }
+
+
+
+    public function show($id)
+    {
+    	$recipe = Recipe::find($id);
+    	return $recipe;
+
+    }
+
+    public function destroy($id)
+    {
+    	Recipe::destroy($id);
+    	return "deleted
+    }
+
+
+
+
+
+
+
+
 }

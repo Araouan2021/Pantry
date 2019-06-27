eac4b4809febf28cb21dd900242f890721b811e6
Bola Bally <bolabally@gmail.com>
2019-06-27 13:44:56 +0000

    addrecipes.blade.php

--- resources/views/recipes.blade.php
+++ resources/views/recipes.blade.php
@@ -62,19 +62,19 @@ h4 {
     <div class="col-md-2"></div>
     <div class="col-md-8">
       <h1 style="text-align: center; color: DarkRed; font-size: 75px; font-family: 'Questrial', sans-serif;, sans-serif; font-weight: bold;">Pantry</h1>
-      <h3 style="color: SaddleBrown; font-family: 'Allura', cursive; font-size: 40px;">Showing Recipes For: &nbsp; $ingredients </h3>
+      <h3 style="color: SaddleBrown; font-family: 'Allura', cursive; font-size: 40px;">Showing Recipes For: &nbsp; ingredients </h3>
       <hr>
       @foreach($recipes as $recipe)
         <div class="recipe-details">
-         <a href="{{route('show', '$args->recipe.id')}}">
-        <img src="route('download', $args->recipe.file)}}"
+         <a href="{{route('recipes', '$recipe.id')}}">
+        <img src="{{route('recipes', '$recipe.file')}}"
              class="img-responsive img-thumbnail">
         </a>
-          <h2 style="color: DarkRed; font-size: 40px; font-family: 'Khand', sans-serif;"></u>Title<br>{{recipe.title}}</h2>
-          <p style="font-family: 'Molengo', sans-serif; font-size: 15px;"><u>Ingredients</u><br>{{recipe.ingredients}}</p>
-          <p style="font-family: 'Molengo', sans-serif; font-size: 15px;"><u>Method</u><br>{{recipe.method}}</p>
-          <p style="font-family: 'Molengo', sans-serif; font-size: 15px;"><u>Serving size</u><br>{{recipe.size}}</p>
-          <p style="font-family: 'Molengo', sans-serif; font-size: 15px;"><u>Time</u><br>{{recipe.time}}</p>
+          <h2 style="color: DarkRed; font-size: 40px; font-family: 'Khand', sans-serif;"></u>Title<br></h2>
+          <p style="font-family: 'Molengo', sans-serif; font-size: 15px;"><u>Ingredients</u><br></p>
+          <p style="font-family: 'Molengo', sans-serif; font-size: 15px;"><u>Method</u><br></p>
+          <p style="font-family: 'Molengo', sans-serif; font-size: 15px;"><u>Serving size</u><br></p>
+          <p style="font-family: 'Molengo', sans-serif; font-size: 15px;"><u>Time</u><br></p>
         </div>
         </div>
       @endforeach
